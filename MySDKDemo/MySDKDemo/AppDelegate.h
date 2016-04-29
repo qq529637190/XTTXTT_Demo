8b1b5f2a89d7addb1665d2b840f3c92ae6567afe
qq529637190 <tyaokaixin@163.com>
2016-04-29 16:20:53 +0800

    add demo

---
+++ MySDKDemo/MySDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySDKDemo
+//
+//  Created by tingting on 16/4/29.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySDKDemo/MySDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySDKDemo
+//
+//  Created by tingting on 16/4/29.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
